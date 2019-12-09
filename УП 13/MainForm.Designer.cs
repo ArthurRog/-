aeab2467a6a404dfbaae14432ec3261942a116cf
ArthurRog <57070089+ArthurRog@users.noreply.github.com>
2019-12-09 20:44:37 +0300

    УП

---
+++ УП 13/MainForm.Designer.cs
@@ -0,0 +1,350 @@
+﻿namespace УП_13
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ButtonAddData = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxQuantityInStock = new System.Windows.Forms.TextBox();
+            this.textBoxManufacturer = new System.Windows.Forms.TextBox();
+            this.textBoxPrice = new System.Windows.Forms.TextBox();
+            this.textBoxKindOfSport = new System.Windows.Forms.TextBox();
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.DeleteButton = new System.Windows.Forms.Button();
+            this.DeleteTextBox1 = new System.Windows.Forms.TextBox();
+            this.textBoxUpdateQuantityInStock = new System.Windows.Forms.TextBox();
+            this.textBoxUpdatePrice = new System.Windows.Forms.TextBox();
+            this.textBoxUpdateManufacturer = new System.Windows.Forms.TextBox();
+            this.textBoxUpdateKindOfSport = new System.Windows.Forms.TextBox();
+            this.textBoxUpdateName = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.UpdateDataButton = new System.Windows.Forms.Button();
+            this.textBoxId = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // ButtonAddData
+            // 
+            this.ButtonAddData.Location = new System.Drawing.Point(27, 256);
+            this.ButtonAddData.Name = "ButtonAddData";
+            this.ButtonAddData.Size = new System.Drawing.Size(200, 29);
+            this.ButtonAddData.TabIndex = 40;
+            this.ButtonAddData.Text = "Добавить";
+            this.ButtonAddData.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(24, 198);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(86, 17);
+            this.label5.TabIndex = 38;
+            this.label5.Text = "Количество";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(24, 153);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(43, 17);
+            this.label4.TabIndex = 37;
+            this.label4.Text = "Цена";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(27, 108);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(110, 17);
+            this.label3.TabIndex = 36;
+            this.label3.Text = "Произвадитель";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(27, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(83, 17);
+            this.label2.TabIndex = 35;
+            this.label2.Text = "Вид спорта";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 17);
+            this.label1.TabIndex = 34;
+            this.label1.Text = "Название";
+            // 
+            // textBoxQuantityInStock
+            // 
+            this.textBoxQuantityInStock.Location = new System.Drawing.Point(27, 218);
+            this.textBoxQuantityInStock.Name = "textBoxQuantityInStock";
+            this.textBoxQuantityInStock.Size = new System.Drawing.Size(200, 22);
+            this.textBoxQuantityInStock.TabIndex = 32;
+            // 
+            // textBoxManufacturer
+            // 
+            this.textBoxManufacturer.Location = new System.Drawing.Point(27, 128);
+            this.textBoxManufacturer.Name = "textBoxManufacturer";
+            this.textBoxManufacturer.Size = new System.Drawing.Size(200, 22);
+            this.textBoxManufacturer.TabIndex = 31;
+            // 
+            // textBoxPrice
+            // 
+            this.textBoxPrice.Location = new System.Drawing.Point(27, 173);
+            this.textBoxPrice.Name = "textBoxPrice";
+            this.textBoxPrice.Size = new System.Drawing.Size(200, 22);
+            this.textBoxPrice.TabIndex = 30;
+            // 
+            // textBoxKindOfSport
+            // 
+            this.textBoxKindOfSport.Location = new System.Drawing.Point(27, 83);
+            this.textBoxKindOfSport.Name = "textBoxKindOfSport";
+            this.textBoxKindOfSport.Size = new System.Drawing.Size(200, 22);
+            this.textBoxKindOfSport.TabIndex = 29;
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Location = new System.Drawing.Point(27, 38);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(200, 22);
+            this.textBoxName.TabIndex = 28;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(513, 19);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(78, 17);
+            this.label7.TabIndex = 43;
+            this.label7.Text = "Введите id";
+            // 
+            // DeleteButton
+            // 
+            this.DeleteButton.Location = new System.Drawing.Point(516, 67);
+            this.DeleteButton.Name = "DeleteButton";
+            this.DeleteButton.Size = new System.Drawing.Size(197, 23);
+            this.DeleteButton.TabIndex = 42;
+            this.DeleteButton.Text = "Удалить";
+            this.DeleteButton.UseVisualStyleBackColor = true;
+            // 
+            // DeleteTextBox1
+            // 
+            this.DeleteTextBox1.Location = new System.Drawing.Point(516, 39);
+            this.DeleteTextBox1.Name = "DeleteTextBox1";
+            this.DeleteTextBox1.Size = new System.Drawing.Size(197, 22);
+            this.DeleteTextBox1.TabIndex = 41;
+            // 
+            // textBoxUpdateQuantityInStock
+            // 
+            this.textBoxUpdateQuantityInStock.Location = new System.Drawing.Point(268, 263);
+            this.textBoxUpdateQuantityInStock.Name = "textBoxUpdateQuantityInStock";
+            this.textBoxUpdateQuantityInStock.Size = new System.Drawing.Size(216, 22);
+            this.textBoxUpdateQuantityInStock.TabIndex = 55;
+            // 
+            // textBoxUpdatePrice
+            // 
+            this.textBoxUpdatePrice.Location = new System.Drawing.Point(268, 218);
+            this.textBoxUpdatePrice.Name = "textBoxUpdatePrice";
+            this.textBoxUpdatePrice.Size = new System.Drawing.Size(216, 22);
+            this.textBoxUpdatePrice.TabIndex = 54;
+            // 
+            // textBoxUpdateManufacturer
+            // 
+            this.textBoxUpdateManufacturer.Location = new System.Drawing.Point(271, 173);
+            this.textBoxUpdateManufacturer.Name = "textBoxUpdateManufacturer";
+            this.textBoxUpdateManufacturer.Size = new System.Drawing.Size(216, 22);
+            this.textBoxUpdateManufacturer.TabIndex = 53;
+            // 
+            // textBoxUpdateKindOfSport
+            // 
+            this.textBoxUpdateKindOfSport.Location = new System.Drawing.Point(271, 128);
+            this.textBoxUpdateKindOfSport.Name = "textBoxUpdateKindOfSport";
+            this.textBoxUpdateKindOfSport.Size = new System.Drawing.Size(216, 22);
+            this.textBoxUpdateKindOfSport.TabIndex = 52;
+            // 
+            // textBoxUpdateName
+            // 
+            this.textBoxUpdateName.Location = new System.Drawing.Point(271, 83);
+            this.textBoxUpdateName.Name = "textBoxUpdateName";
+            this.textBoxUpdateName.Size = new System.Drawing.Size(216, 22);
+            this.textBoxUpdateName.TabIndex = 51;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(268, 18);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(219, 17);
+            this.label8.TabIndex = 50;
+            this.label8.Text = "Введите id для редактирования";
+            // 
+            // UpdateDataButton
+            // 
+            this.UpdateDataButton.Location = new System.Drawing.Point(268, 291);
+            this.UpdateDataButton.Name = "UpdateDataButton";
+            this.UpdateDataButton.Size = new System.Drawing.Size(216, 29);
+            this.UpdateDataButton.TabIndex = 49;
+            this.UpdateDataButton.Text = "Редактировать";
+            this.UpdateDataButton.UseVisualStyleBackColor = true;
+            // 
+            // textBoxId
+            // 
+            this.textBoxId.Location = new System.Drawing.Point(271, 38);
+            this.textBoxId.Name = "textBoxId";
+            this.textBoxId.Size = new System.Drawing.Size(216, 22);
+            this.textBoxId.TabIndex = 48;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(268, 63);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(72, 17);
+            this.label6.TabIndex = 63;
+            this.label6.Text = "Название";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(268, 108);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(83, 17);
+            this.label9.TabIndex = 64;
+            this.label9.Text = "Вид спорта";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(268, 153);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(110, 17);
+            this.label10.TabIndex = 65;
+            this.label10.Text = "Произвадитель";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(265, 198);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(43, 17);
+            this.label11.TabIndex = 66;
+            this.label11.Text = "Цена";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(265, 243);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(86, 17);
+            this.label12.TabIndex = 67;
+            this.label12.Text = "Количество";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(950, 564);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBoxUpdateQuantityInStock);
+            this.Controls.Add(this.textBoxUpdatePrice);
+            this.Controls.Add(this.textBoxUpdateManufacturer);
+            this.Controls.Add(this.textBoxUpdateKindOfSport);
+            this.Controls.Add(this.textBoxUpdateName);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.UpdateDataButton);
+            this.Controls.Add(this.textBoxId);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.DeleteButton);
+            this.Controls.Add(this.DeleteTextBox1);
+            this.Controls.Add(this.ButtonAddData);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxQuantityInStock);
+            this.Controls.Add(this.textBoxManufacturer);
+            this.Controls.Add(this.textBoxPrice);
+            this.Controls.Add(this.textBoxKindOfSport);
+            this.Controls.Add(this.textBoxName);
+            this.Name = "MainForm";
+            this.Text = "MainForm";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ButtonAddData;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxQuantityInStock;
+        private System.Windows.Forms.TextBox textBoxManufacturer;
+        private System.Windows.Forms.TextBox textBoxPrice;
+        private System.Windows.Forms.TextBox textBoxKindOfSport;
+        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button DeleteButton;
+        private System.Windows.Forms.TextBox DeleteTextBox1;
+        private System.Windows.Forms.TextBox textBoxUpdateQuantityInStock;
+        private System.Windows.Forms.TextBox textBoxUpdatePrice;
+        private System.Windows.Forms.TextBox textBoxUpdateManufacturer;
+        private System.Windows.Forms.TextBox textBoxUpdateKindOfSport;
+        private System.Windows.Forms.TextBox textBoxUpdateName;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button UpdateDataButton;
+        private System.Windows.Forms.TextBox textBoxId;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+    }
+}
+
---
+++ УП 13/MainForm.cs
@@ -0,0 +1,92 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace УП_13
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+            ButtonAddData.Click += AddData;
+            DeleteButton.Click += DeleteData;
+            UpdateDataButton.Click += UpdateData;
+        }
+
+        public void UpdateData(object sender, EventArgs e)
+        {
+            DataBaseSQL dataBase = new DataBaseSQL();
+
+            MySqlCommand command = new MySqlCommand("UPDATE product SET Name = @Name, KindOfSport = @KindOfSport, Manufacturer = @Manufacturer, Price= @Price, QuantityInStock = @QuantityInStock WHERE id = @id", dataBase.getConnection());
+            command.Parameters.Add("@id", MySqlDbType.VarChar).Value = textBoxId.Text;
+
+            command.Parameters.Add("@Name", MySqlDbType.VarChar).Value = textBoxUpdateName.Text;
+            command.Parameters.Add("@KindOfSport", MySqlDbType.VarChar).Value = textBoxUpdateKindOfSport.Text;
+            command.Parameters.Add("@Manufacturer", MySqlDbType.VarChar).Value = textBoxUpdateManufacturer.Text;
+            command.Parameters.Add("@Price", MySqlDbType.VarChar).Value = textBoxUpdatePrice.Text;
+            command.Parameters.Add("@QuantityInStock", MySqlDbType.VarChar).Value = textBoxUpdateQuantityInStock.Text;
+            dataBase.openConnection();
+
+            command.ExecuteNonQuery();
+
+            dataBase.closeConnection();
+        }
+
+        public void AddData(object sender, EventArgs e)
+        {
+            if (textBoxName.Text != "" && textBoxKindOfSport.Text != "" && textBoxManufacturer.Text != "" &&
+                textBoxPrice.Text != "" && textBoxQuantityInStock.Text != "")
+            {
+                DataBaseSQL dataBase = new DataBaseSQL();
+                MySqlCommand command = new MySqlCommand("INSERT INTO product (Name, KindOfSport, Manufacturer, Price, QuantityInStock) VALUES (@Name, @KindOfSport, @Manufacturer, @Price, @QuantityInStock)", dataBase.getConnection());
+
+                command.Parameters.Add("@Name", MySqlDbType.VarChar).Value = textBoxName.Text;
+                command.Parameters.Add("@KindOfSport", MySqlDbType.VarChar).Value = textBoxKindOfSport.Text;
+                command.Parameters.Add("@Manufacturer", MySqlDbType.VarChar).Value = textBoxManufacturer.Text;
+                command.Parameters.Add("@Price", MySqlDbType.VarChar).Value = textBoxPrice.Text;
+                command.Parameters.Add("@QuantityInStock", MySqlDbType.VarChar).Value = textBoxQuantityInStock.Text;
+
+                dataBase.openConnection();
+
+                if (command.ExecuteNonQuery() == 1)
+                {
+                    MessageBox.Show("Данные добавленны");
+                }
+                else MessageBox.Show("Данные не добавленны");
+
+                dataBase.closeConnection();
+            }
+            else MessageBox.Show("Данные введены некорректно");
+        }
+
+        public void DeleteData(object sender, EventArgs e)
+        {
+            DataBaseSQL dataBase = new DataBaseSQL();
+            dataBase.openConnection();
+
+            MySqlCommand command = new MySqlCommand("DELETE FROM product WHERE id = @id", dataBase.getConnection());
+            command.Parameters.Add("@id", MySqlDbType.VarChar).Value = DeleteTextBox1.Text;
+
+            if (command.ExecuteNonQuery() == 1)
+            {
+                MessageBox.Show("Данные удалены");
+            }
+            else MessageBox.Show("Данные не удалены");
+
+            dataBase.closeConnection();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
